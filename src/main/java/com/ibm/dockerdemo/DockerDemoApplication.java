49c0b733725f4e97a4b501841f93329a5fefc07f
Abhishek Sawhney <absawhn1@in.ibm.com>
2020-02-12 11:45:19 +0530

    Initial commit

---
+++ src/main/java/com/ibm/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,27 @@
+package com.ibm.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@GetMapping("/v1/hello")
+	public String helloWorld() {
+	
+		return "hellow world 2";
+	}
+
+}
